d55aeb2da13778b40f738253d27ce79560d07be0
Aldo <aldorgarcia29@gmail.com>
2022-03-29 11:51:00 -0400

    refactor

--- src/App.js
+++ src/App.js
@@ -24,32 +24,46 @@ const list = [
   },
 ];
 
-function App() {
+const App = () => {
   return (
     <div>
       <h1>
         {welcome.greeting}
         {welcome.title}
       </h1>
-      <label htmlFor="search">Search:</label>
-      <input id="search" type="text" />
+      <Search />
       <hr />
-      <div>
-        {list.map((item) => {
-          return (
-            <li key={item.objectID}>
-              <span>
-                <a href={item.url}>{item.title}</a>
-              </span>
-              <span> {item.author}</span>
-              <span> {item.num_comments}</span>
-              <span> {item.points}</span>
-            </li>
-          );
-        })}
-      </div>
+      <List />
     </div>
   );
-}
+};
+
+const List = () => {
+  return (
+    <div>
+      {list.map((item) => {
+        return (
+          <li key={item.objectID}>
+            <span>
+              <a href={item.url}>{item.title}</a>
+            </span>
+            <span> {item.author}</span>
+            <span> {item.num_comments}</span>
+            <span> {item.points}</span>
+          </li>
+        );
+      })}
+    </div>
+  );
+};
+
+const Search = () => {
+  return (
+    <div>
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" />
+    </div>
+  );
+};
 
 export default App;
